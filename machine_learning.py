e14f474336ce419758bd41effbb8b120e3b0a5f4
droidy12527 <gundiaramasu@gmail.com>
2020-03-07 22:02:03 +0530

    Version 1.1

--- machine_learning.py
+++ machine_learning.py
@@ -2,17 +2,20 @@ import numpy as np
 import pandas as pd 
 import seaborn as se 
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn import ensemble
 data = pd.read_csv('data_housing.csv')
-plt.scatter(data['price'],data['sqft_living'])
-plt.title('Price vs Squarefeet')
-plt.show()
+ans = input('Do you want to see graph for the dataset ?')
+if ans.lower() == 'yes':
+    plt.scatter(data['price'],data['sqft_living'])
+    plt.title('Price vs Squarefeet')
+    plt.show()
 y = data['price']
 date_conversion = [1 if values == 2014 else 0 for values in data.date]
 data['date'] = date_conversion
 X = data.drop(['id' , 'price'], axis=1)
 X_train , X_test , y_train, y_test = train_test_split(X,y, random_state=2,test_size=0.10 )
-reg = LinearRegression()
-reg.fit(X_train, y_train)
-print(reg.score(X_test, y_test))
+clf = ensemble.GradientBoostingRegressor(n_estimators = 400, max_depth = 5, min_samples_split = 2,learning_rate = 0.1, loss = 'ls')
+clf.fit(X_train, y_train)
+print('Accuracy = ' + str(clf.score(X_test,y_test)*100))
+
